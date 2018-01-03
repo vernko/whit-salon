d7eaceac3c807efea758f5bee2f14fb5352630ac
Vern Kofford <cheow3@gmail.com>
2018-01-03 15:21:46 -0700

    Customized portfolio, services, and transformations routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  resources :services
-  resources :transformations
-  resources :portfolios
-  resources :blogs
+  resources :services, except: [:show]
+  get 'service/:id', to: 'services#show', as: 'service_show'
+
+  resources :transformations, except: [:show]
+  get 'transformation/:id', to: 'transformations#show', as: 'transformation_show'
+
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+ 
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
+  resources :blogs
   
   root to: 'pages#home'
 end
